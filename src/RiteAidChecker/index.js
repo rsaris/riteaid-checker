00d526979a76716602d3ae33f1326f84e1735b71
Bob Saris <bob@bobsaris.com>
2021-02-19 21:20:37 -0500

    Hello world

---
+++ src/RiteAidChecker/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import {
+  Text,
+  View,
+} from 'react-native';
+
+export default function RightAidChecker() {
+  return (
+    <View>
+      <Text>Hello world!</Text>
+    </View>
+  );
+}
